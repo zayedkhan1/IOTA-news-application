25d77926b875685ac2ff102a15b4ebcb3daa4abe
zayed khan <zayedk458@gmail.com>
2025-07-27 23:13:50 +0600

    3rd commit

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = ({ setCatagory }) => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16 relative">
 
-            {/* Logo */}
+            {/* nav  Logo */}
             <a href='/' className="flex-shrink-0 text-2xl font-bold text-emerald-400">
               IOTA News
             </a>
